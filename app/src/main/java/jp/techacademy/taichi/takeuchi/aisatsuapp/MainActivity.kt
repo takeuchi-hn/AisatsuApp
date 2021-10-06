7ab7323ae76a22f849b9ef1c151181d05912fb22
takeuchi-hn <sinnichi406t.t@gmail.com>
2021-10-07 01:03:03 +0900

    totyuu

--- app/src/main/java/jp/techacademy/taichi/takeuchi/aisatsuapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/taichi/takeuchi/aisatsuapp/MainActivity.kt
@@ -1,11 +1,46 @@
 package jp.techacademy.taichi.takeuchi.aisatsuapp
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+
     }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button1) {
+            showTimePickerDialog()
+            Log.d("UI_PARTS", "ボタン押した")
+        }
+    }
+
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+                this,
+                TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                    Log.d("UI-PARTS", "$hour:$minute")
+                },
+                13, 0, true)
+        timePickerDialog.show()
+        /*if (timePickerDialog$hour >= 18 || timePickerDialog.hour == 1){
+            textView.text = "こんばんわ"
+        }else　if(timePickerDialog.hour >= 10){
+            textView.text = "こんにちわ"
+        }else　if(timePickerDialog.hour >= 2){
+            textView.text = "おはよう"
+        }:*/
+    }
+
+
 }
